0f5b35d29143bbe701a36946ac31036245021579
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-04-03 23:40:07 +0530

    initial commit

---
+++ 01-getting-started/hello_world/main.rs
@@ -0,0 +1,10 @@
+// $ mkdir ~/projects
+// $ cd ~/projects
+// $ mkdir hello_world
+// $ cd hello_world
+// $ rustc main.rs
+// $ ./main       
+
+fn main() {
+    println!("Hello World! 2");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust 0 to 100
+
+This repository documents my journey and progress in learning the Rust programming language.
